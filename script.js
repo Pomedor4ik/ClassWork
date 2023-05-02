613f7482c76017b1482918db0e99e43b205ec67d
Pomedor4ik <fpomedor4ik@gmail.com>
2023-05-02 17:20:45 +0300

    done

--- script.js
+++ script.js
@@ -30,30 +30,45 @@ let productsStore = [
     {
         id:1,
         name:"TV",
-        price: 3000
+        price: 3000,
+        count: 7
     },
     {
         id:2,
         name: "Phone",
-        price: 5000
+        price: 5000,
+        count:5
     },
     {
         id:3,
         name: "PC", 
-        price: 10000
+        price: 10000,
+        count:6
     }
 ]
 
 let bucketStore = []
+
+
 
 const removeProduct = (idToRemove) => { 
     productsStore = productsStore.filter(product => {
         if (product.id != idToRemove){
             return product
         }
+        if (product.count > 0){
+            product.count = product.count-1
+            return product
+        }
     })
     generateProducts()
 }
+
+
+
+
+
+// =
 
 const removeBucket = (idToRemove) => {
     bucketStore = bucketStore.filter(bucketItem => {
@@ -77,21 +92,68 @@ const findItemInArray = (array, id) => {
 const generateProducts = () => {
     productsList.innerHTML = ''
     productsStore.forEach(product => {
+        let price = product.price
+        let count = product.count
+
+        if (count < 3) {
+            price *= 1.2
+        }
+
         productsList.innerHTML += `
         <div id="product-${product.id}" class="product">
             <p class="name">${product.name}</p>
-            <p class="price">${product.price}</p>
+            <p class="price">${price}</p>
             <button class="btnAdd">Add to bucket</button>
+            <p class="count">Count:${count}</p>
         </div>
         `
     })
+
+    // Add button "Add all to bucket"
+    productsList.innerHTML += `
+        <div class="product">
+            <button class="btnAddAll">Add all to bucket</button>
+        </div>
+    `;
+
+    // Add event listener for "Add all to bucket" button
+    const btnAddAll = document.querySelector('.btnAddAll');
+    btnAddAll.addEventListener('click', () => {
+        let balanceCurrent = parseInt(localStorage.getItem('balance'));
+        let productsToAdd = productsStore.filter(product => product.count > 0);
+
+        productsToAdd.forEach(product => {
+            let price = product.price;
+
+            if (product.count < 3) {
+                price *= 1.2;
+            }
+
+            if (balanceCurrent - price >= 0) {
+                bucketStore = [
+                    ...bucketStore,
+                    {...product}
+                ];
+                balanceCurrent -= price;
+                product.count -= 1;
+            } else { 
+                alert("You can't add this product on bucket");
+            }
+        });
+
+        setBalance(balanceCurrent);
+        generateBucket();
+        generateProducts();
+    });
+
     let btnsAdd = document.querySelectorAll('.btnAdd')
     btnsAdd.forEach((btnItem) => { 
         btnItem.addEventListener('click', () => { 
             let currentId = parseInt(btnItem.parentNode.id.substring(8))
             let balanceCurrent = parseInt(localStorage.getItem('balance'))
             let price = productsStore[findItemInArray(productsStore, currentId)].price
-            if (balanceCurrent - price >= 0) {
+            let count = productsStore[findItemInArray(productsStore, currentId)].count
+            if (balanceCurrent - price >= 0 && count > 0) {
                 bucketStore = [
                     ...bucketStore,
                     {...productsStore[findItemInArray(productsStore, currentId)]}
@@ -105,6 +167,8 @@ const generateProducts = () => {
         })
     })
 }
+
+// =
 
 const generateBucket = () => { 
     bucketList.innerHTML = ''
